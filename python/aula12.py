e02bcce9af358115421d6ce41866eb33af120e10
vicped1 <149419147+vicped1@users.noreply.github.com>
2024-12-10 09:06:26 -0300

    f-Strings

---
+++ python/aula12.py
@@ -0,0 +1,7 @@
+variavel = 'ABCD'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}')
+print(f'{variavel: ^10}')
+print(f'{1000.4871248466989:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
