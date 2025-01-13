4619acbcdac81939b9df0be5552a5dc76dc3eec0
vicped1 <149419147+vicped1@users.noreply.github.com>
2025-01-13 13:49:27 -0300

    exercicioGuiadoCalc

---
+++ python/exercicioGuiaddoCalc.py
@@ -0,0 +1,47 @@
+while True:
+    primeiroNumero = input('digite um numero: ')
+    segundoNumero = input('digite outro numero: ')
+    operador = input('digite um operadores (+-/*): ')
+
+    numeros_validos = None
+
+    try:
+        num_1 = float(primeiroNumero)
+        num_2 = float(segundoNumero)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+    
+    if numeros_validos is None:
+        print('digite numeros validos')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('digite um operador permitido')
+        continue
+
+    if len(operador) > 1:
+        print('digite apenas um operador')
+        continue
+
+    if operador == '+':
+        print(f'{num_1 + num_2}')
+    
+    if operador == '-':
+        print(f'{num_1 - num_2}')
+
+    if operador == '/':
+        print(f'{num_1 / num_2}')
+
+    if operador == '*':
+        print(f'{num_1 * num_2}')
+    
+
+
+    sair = input('quer sair? [s] pra sim ').lower().startswith('s')
+    
+    if sair is True:
+        break
+
