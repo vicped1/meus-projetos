97bafb8edcbbc94d56287e6ac6268b571ab0d2a5
vicped1 <149419147+vicped1@users.noreply.github.com>
2025-01-21 12:14:56 -0300

    aula31

---
+++ python/aula31.py
@@ -0,0 +1,11 @@
+#nome = 'Luiz'
+#outra_variavel = nome
+#nome = 'Pedro'
+#print(nome)
+#print(outra_variavel)
+
+lista_a = ['Pedro', 'Luiz', 'Maria', 1, False, 2.14]
+lista_b = lista_a.copy()
+lista_a[0] = 'Qualquer coisa'
+print(lista_a) 
+print(lista_b)
