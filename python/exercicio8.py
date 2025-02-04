e9345474f9b3367888771e7f208d3b39cf3b8223
vicped1 <149419147+vicped1@users.noreply.github.com>
2025-02-04 15:15:04 -0300

    b

--- python/exercicio8.py
+++ python/exercicio8.py
@@ -1,7 +1,7 @@
-cpf = '34877451005'
-nove_digito = cpf[:9]
+cpf_enviado = '34877451005'
+nove_digito = cpf_enviado[:9]
 contador_regressivo_1 = 10
-dez_digito = cpf[:10]
+dez_digito = cpf_enviado[:10]
 contador_regressivo_2 = 11
 
 resultado_digito_1 = 0
@@ -12,14 +12,14 @@ for digito in nove_digito:
     contador_regressivo_1 -= 1
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
-print(digito_1)
 
 for digito in dez_digito:
     resultado_digito_2 += int(digito) * contador_regressivo_2
     contador_regressivo_2 -= 1
 digito_2 = (resultado_digito_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
-print(digito_2)
 
-novo_cpf = f'{nove_digito}{digito_1}{digito_2}'
-print(novo_cpf)
+cpf_gerado = f'{nove_digito}{digito_1}{digito_2}'
+
+if cpf_enviado == cpf_gerado:
+    print(f'o cpf enviado {cpf_enviado} é válido')
