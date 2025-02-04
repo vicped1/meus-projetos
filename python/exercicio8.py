acaea6409ef36d5c0df2c9f474cc7e3aca0d87b6
vicped1 <149419147+vicped1@users.noreply.github.com>
2025-02-04 15:12:08 -0300

    validação cpf

--- python/exercicio8.py
+++ python/exercicio8.py
@@ -7,16 +7,19 @@ contador_regressivo_2 = 11
 resultado_digito_1 = 0
 resultado_digito_2 = 0
 
-for digito_1 in nove_digito:
-    resultado_digito_1 += int(digito_1) * contador_regressivo_1
+for digito in nove_digito:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
     contador_regressivo_1 -= 1
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
 print(digito_1)
 
-for digito_2 in dez_digito:
-    resultado_digito_2 += int(digito_2) * contador_regressivo_2
+for digito in dez_digito:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
     contador_regressivo_2 -= 1
 digito_2 = (resultado_digito_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
 print(digito_2)
+
+novo_cpf = f'{nove_digito}{digito_1}{digito_2}'
+print(novo_cpf)
